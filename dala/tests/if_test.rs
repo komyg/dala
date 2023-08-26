978b43387eeec29cc9f653827264c5b85ee745a9
Felipe Armoni <felipe.armoni@gmail.com>
2023-08-26 11:00:41 -0300

    Added if test

--- dala/tests/if_test.rs
+++ dala/tests/if_test.rs
@@ -1,4 +1,6 @@
-use dala::{eval, DalaValue};
+use std::collections::HashMap;
+
+use dala::{eval, eval_with_data, DalaValue};
 
 #[test]
 fn test_if_true() {
@@ -52,4 +54,36 @@ fn test_neq_returns_false() {
 
     let DalaValue::Str(value) = result[0].as_ref().unwrap() else { panic!("Not a string") };
     assert_eq!(value.to_owned(), "Hello");
+}
+
+#[test]
+fn test_with_data() {
+    let result = eval_with_data(
+        "IF(EQ($arg1, $arg2), $foo, $bar)",
+        &HashMap::from([
+            ("$arg1", &DalaValue::Num(1.0)),
+            ("$arg2", &DalaValue::Num(1.0)),
+            ("$foo", &DalaValue::Str("foo".to_string())),
+            ("$bar", &DalaValue::Str("bar".to_string())),
+        ]),
+    );
+    assert_eq!(result.len(), 1);
+
+    let DalaValue::Str(value) = result[0].as_ref().unwrap() else { panic!("Not a string") };
+    assert_eq!(value.to_owned(), "foo");
+}
+
+#[test]
+fn test_with_mixed_data() {
+    let result = eval_with_data(
+        "IF(EQ($arg1, 1.0), \"foo\", $bar)",
+        &HashMap::from([
+            ("$arg1", &DalaValue::Num(1.0)),
+            ("$bar", &DalaValue::Str("bar".to_string())),
+        ]),
+    );
+    assert_eq!(result.len(), 1);
+
+    let DalaValue::Str(value) = result[0].as_ref().unwrap() else { panic!("Not a string") };
+    assert_eq!(value.to_owned(), "foo");
 }
