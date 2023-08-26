0c497cfa8f62e2de4546aabdfacfa9c37c835192
Felipe Armoni <felipe.armoni@gmail.com>
2023-08-26 10:34:39 -0300

    Added SUM test

--- dala/tests/sum_test.rs
+++ dala/tests/sum_test.rs
@@ -1,4 +1,5 @@
-use dala::{eval, DalaError, DalaValue};
+use dala::{eval, eval_with_data, DalaError, DalaValue};
+use std::collections::HashMap;
 
 #[test]
 fn test_sum() {
@@ -25,4 +26,18 @@ fn test_sum_runtime_error_with_str_arg() {
 
     let DalaError::RuntimeError(err) = result[0].as_ref().unwrap_err() else { panic!("Not a number") };
     assert_eq!(err.message, "Cannot sum strings, found: 2");
+}
+
+#[test]
+fn test_with_data() {
+    let data = &HashMap::from([
+        ("$othervar", &DalaValue::Num(3.0)),
+        ("$1", &DalaValue::Num(1.5)),
+        ("$2", &DalaValue::Num(2.0)),
+    ]);
+    let result = eval_with_data("SUM($1, $2, $othervar, 4, -5)", data);
+    assert_eq!(result.len(), 1);
+
+    let DalaValue::Num(value) = result[0].as_ref().unwrap() else { panic!("Not a number") };
+    assert_eq!(value.to_owned(), 5.5);
 }
